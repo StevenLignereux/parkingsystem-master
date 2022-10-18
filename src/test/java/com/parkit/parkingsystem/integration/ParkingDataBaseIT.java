12f450cbb31396b50d394abcbf132c1a2019cbe4
Steven Lignereux <slignere@gmail.com>
2022-10-18 15:12:25 +0200

    Add Integrations tests for ParkingDataBaseIT : - check that a ticket is actualy saved in DB and Parking table is updated with availability - check that the fare generated and out time are populated correctly in the database

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,12 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +16,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+import static junit.framework.Assert.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,14 +59,39 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        Ticket testTicket = ticketDAO.getTicket("ABCDEF");
+
+        assertNotNull(testTicket);
+
+        int nextSlot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+        assertNotSame(1, nextSlot);
     }
 
     @Test
     public void testParkingLotExit(){
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        // generate a ticket
+        Ticket ticket = new Ticket();
+
+        ticket.setInTime(new Date(System.currentTimeMillis() - 60 * 60 * 1000));
+        ticket.setOutTime(null);
+        ticket.setPrice(0);
+        ticket.setVehicleRegNumber("ABCDEF");
+        ticket.setId(1);
+        ticket.setParkingSpot(new ParkingSpot(1, ParkingType.CAR, false));
+
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+        Ticket testTicket = ticketDAO.getTicket("ABCDEF");
+        assertNotSame(null, testTicket.getOutTime());
+        assertNotSame(0, testTicket.getPrice());
+
+        ticketDAO.saveTicket(testTicket);
+        parkingSpotDAO.updateParking(testTicket.getParkingSpot());
+
+        parkingService.processExitingVehicle();
     }
 
 }
