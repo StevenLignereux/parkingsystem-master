d353698c45d9423d7097cef400f366bdfac6afcd
Steven Lignereux <slignere@gmail.com>
2022-11-11 09:55:11 +0100

    add test for calculate fare with more than a day with discount

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -262,5 +262,36 @@ public class FareCalculatorServiceTest {
         assertEquals((0.75 * Fare.CAR_RATE_PER_HOUR * Fare.DISCOUNT_FOR_REGULAR_USER), ticket.getPrice());
     }
 
+    @Test
+    public void calculateFareCarWithMoreThanADayParkingTimeWithDiscount() {
+        Date inTime = new Date();
+        inTime.setTime(System.currentTimeMillis() - (24 * 60 * 60 * 1000));//24 hours parking time should give 24 * parking fare per hour
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setAlreadyCame(true);
+
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals((24 * Fare.CAR_RATE_PER_HOUR * Fare.DISCOUNT_FOR_REGULAR_USER), ticket.getPrice());
+    }
+    @Test
+    public void calculateFareBikeWithMoreThanADayParkingTimeWithDiscount() {
+        Date inTime = new Date();
+        inTime.setTime(System.currentTimeMillis() - (24 * 60 * 60 * 1000));//24 hours parking time should give 24 * parking fare per hour
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setAlreadyCame(true);
+
+
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals((24 * Fare.BIKE_RATE_PER_HOUR * Fare.DISCOUNT_FOR_REGULAR_USER), ticket.getPrice());
+    }
 
 }
